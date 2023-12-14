bf7309b9f42a7cbb2e156e30444b32265e0dd9f9
방찬석 <dipsy1014@naver.com>
2023-12-14 17:38:46 +0900

    초기 커밋.

---
+++ OPCTest/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace OPCTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NodesTree = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NodesTree
+            // 
+            this.NodesTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NodesTree.Location = new System.Drawing.Point(3, 3);
+            this.NodesTree.Name = "NodesTree";
+            this.NodesTree.Size = new System.Drawing.Size(400, 589);
+            this.NodesTree.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.NodesTree, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(813, 595);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 595);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView NodesTree;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ OPCTest/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Opc.Ua;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OPCTest
+{
+    public partial class Form1 : Form
+    {
+
+        Opc.UaFx.Client.OpcClient _pOpcClient = new OpcClient();
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+
+                _pOpcClient = new Opc.UaFx.Client.OpcClient("opc.tcp://"+ "ip:port");
+
+                //Opc.UaFx.Client.OpcClient opcClient = new Opc.UaFx.Client.OpcClient("opc.tcp://192.168.0.241:56000");
+                _pOpcClient.Security.AutoUpgradeEndpointPolicy = false;
+                _pOpcClient.Security.AutoAcceptUntrustedCertificates = true;
+                _pOpcClient.Configuration.ApplicationUri = "test";
+
+                _pOpcClient.ReconnectTimeout = 2000;//재연결 타임아웃
+
+                _pOpcClient.OperationTimeout = 2000;//연결 타임아웃
+
+                _pOpcClient.DisconnectTimeout = 2000;
+
+
+                #region ○ 보안정책 ( Security Policy )
+                /*
+                Aes128_Sha256_RsaOaep (not in .NET Framework, COM or Excel, i.e. only in .NET Standard)
+                http://opcfoundation.org/UA/SecurityPolicy#Aes128_Sha256_RsaOaep
+
+                Aes256_Sha256_RsaPss (not in .NET Framework, COM or Excel, i.e. only in .NET Standard)
+                http://opcfoundation.org/UA/SecurityPolicy#Aes256_Sha256_RsaPss
+
+                Basic128Rsa15 (obsolete)
+                http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15
+
+                Basic256	
+                http://opcfoundation.org/UA/SecurityPolicy#Basic256
+
+                Basic256Sha256	
+                http://opcfoundation.org/UA/SecurityPolicy#Basic256Sha256
+                 
+                None	
+                http://opcfoundation.org/UA/SecurityPolicy#None
+                 */
+                #endregion
+
+                if (_pOpcClient.State.ToString() != "Connected" && _pOpcClient.State.ToString() != "Reconnected" && _pOpcClient.State.ToString() != "Reconnecting" && _pOpcClient.State.ToString() != "Connecting")
+                {
+                    _pOpcClient.Connect();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            TreenodeSetting();
+        }
+
+        private void TreenodeSetting()
+        {
+            try
+            {
+
+                OpcBrowseNode obn = new OpcBrowseNode(OpcObjectTypes.ObjectsFolder, OpcBrowseNodeDegree.Generation,
+                    referenceTypes: new[] {
+                            OpcReferenceType.Organizes,
+                            OpcReferenceType.HasComponent,
+                            OpcReferenceType.HasProperty,
+
+                    });
+
+                var node = _pOpcClient.BrowseNode(obn);
+                TreeNode tn = OpcUaFindTreeNodes(node);
+                NodesTree.Nodes.Add(tn);
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        #region ○ 전체노드 찾기
+        private TreeNode OpcUaFindTreeNodes(OpcNodeInfo node)
+        {
+            TreeNode parentTree = (new TreeNode(node.Name.ToString()));
+            NodesTree.Nodes.Add(parentTree);
+
+            foreach (var childNode in node.Children())
+            {
+                TreeNode childTree = new TreeNode();
+                childTree.Text = childNode.Name.ToString();
+
+                if (childNode.Children().Count() > 0)
+                {
+                    ReqNode(childNode, childTree);
+                }
+
+                NodesTree.Nodes.Add(childTree);
+            }
+            return parentTree;
+        }
+
+        private static void ReqNode(OpcNodeInfo childNode, TreeNode childTree)
+        {
+            foreach (var childNode2 in childNode.Children())
+            {
+                TreeNode childTree2 = new TreeNode();
+                childTree2.Text = childNode2.Name.ToString();
+                childTree.Nodes.Add(childTree2);
+
+                if (childNode2.Children().Count() > 0)
+                {
+                    ReqNode(childNode2, childTree2);
+                }
+            }
+        }
+        #endregion
+
+      
+    }
+}
